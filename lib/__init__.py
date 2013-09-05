7e1274bf91b41310310e0ddcab2e451c4db4695c
Sebastian Riese <s.riese@zombofant.net>
2013-09-05 14:44:11 +0200

    refactoring

--- lib/__init__.py
+++ lib/__init__.py
@@ -3,3 +3,8 @@ import sys
 import os
 
 data_dir = os.path.join(sys.prefix, "share/pyxtrlock")
+
+def panic(message, exit_code=1):
+    """Print an error message to stderr and exit"""
+    print(message, file=sys.stderr)
+    sys.exit(exit_code)
