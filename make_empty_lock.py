6f1c0ddc001c10d9e200aa2eeb80283bf2b1b591
Alexander Klink <git@alech.de>
2015-01-15 12:23:49 +0100

    script to make empty lock cursor

---
+++ make_empty_lock.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import pickle
+import sys
+
+fg_bitmap = bytes([0x00])
+bg_bitmap = bytes([0x00])
+
+with open("lock.pickle", "wb") as f:
+    pickle.dump({
+            "width": 1,
+            "height": 1,
+            "x_hot": 1,
+            "y_hot": 1,
+            "fg_bitmap": fg_bitmap,
+            "bg_bitmap": bg_bitmap,
+            "color_mode": "named",
+            "bg_color": "steelblue3",
+            "fg_color": "grey25"
+    }, f)
