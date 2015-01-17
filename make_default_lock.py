eb90b1382021c83bfc297bb5b88418606bb1aa50
Leon Weber <leon@leonweber.de>
2015-01-17 22:41:00 +0100

    Get rid of useless import and fix indentation

--- make_default_lock.py
+++ make_default_lock.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import pickle
-import sys
 
 fg_bitmap = bytes([
     0xff, 0xff, 0xff, 0xff, 0xff, 0x01, 0xf8, 0xff, 0x7f, 0x00, 0xe0, 0xff,
@@ -39,13 +38,13 @@ bg_bitmap = bytes([
 
 with open("lock.pickle", "wb") as f:
     pickle.dump({
-            "width": 28,
-            "height": 40,
-            "x_hot": 14,
-            "y_hot": 21,
-            "fg_bitmap": fg_bitmap,
-            "bg_bitmap": bg_bitmap,
-            "color_mode": "named",
-            "bg_color": "steelblue3",
-            "fg_color": "grey25"
+        "width": 28,
+        "height": 40,
+        "x_hot": 14,
+        "y_hot": 21,
+        "fg_bitmap": fg_bitmap,
+        "bg_bitmap": bg_bitmap,
+        "color_mode": "named",
+        "bg_color": "steelblue3",
+        "fg_color": "grey25"
     }, f)
