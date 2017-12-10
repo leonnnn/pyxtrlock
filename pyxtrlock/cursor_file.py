01a747bd2c4a86ffe5e04dcca690a5413549b2e3
Sebastian Riese <s.riese@zombofant.net>
2017-12-10 14:29:42 +0100

    make the cursor file checking more strict

--- pyxtrlock/cursor_file.py
+++ pyxtrlock/cursor_file.py
@@ -7,11 +7,20 @@ INTEGER_ATTRIBUTES = ["width", "height", "x_hot", "y_hot"]
 COLOR_ATTRIBUTES = ["bg_color", "fg_color"]
 BINARY_ATTRIBUTES = ["fg_bitmap", "bg_bitmap"]
 
+MAX_CURSOR_SIZE = 512
+
+def _check_size(w, h, data):
+    pitch = (w + 7) // 8
+    size = pitch * h
+    if len(data) < size:
+        raise ValueError("invalid cursor file: bitmap data is too small")
+
 
 def save_cursor(cursor, f):
     res = {}
     for attr in INTEGER_ATTRIBUTES:
-        if not isinstance(cursor[attr], int):
+        if not (isinstance(cursor[attr], int) and
+                0 <= cursor[attr] <= MAX_CURSOR_SIZE):
             raise ValueError("{} must be integer".format(attr))
         res[attr] = cursor[attr]
 
@@ -22,8 +31,12 @@ def save_cursor(cursor, f):
             raise ValueError("invalid color specification")
         res[attr] = cursor[attr]
 
+    _check_size(cursor["width"], cursor["height"], cursor["fg_bitmap"])
+    _check_size(cursor["width"], cursor["height"], cursor["bg_bitmap"])
+
     for attr in BINARY_ATTRIBUTES:
         res[attr] = base64.b64encode(cursor[attr]).decode("ascii")
+
     json.dump(res, f, sort_keys=True)
 
 
@@ -32,7 +45,8 @@ def load_cursor(f):
     res = {}
 
     for attr in INTEGER_ATTRIBUTES:
-        if not isinstance(cursor[attr], int):
+        if not (isinstance(cursor[attr], int)  and
+                0 <= cursor[attr] <= MAX_CURSOR_SIZE):
             raise ValueError("{} must be integer".format(attr))
         res[attr] = cursor[attr]
 
@@ -45,5 +59,8 @@ def load_cursor(f):
 
     for attr in BINARY_ATTRIBUTES:
         res[attr] = base64.b64decode(cursor[attr])
+
+    _check_size(res["width"], res["height"], res["fg_bitmap"])
+    _check_size(res["width"], res["height"], res["bg_bitmap"])
 
     return res
